6d9a85723ff7bc0fc42bff02648e4cc0336224ce
MarianaAvila <mavila@oswego.edi>
2023-04-28 14:20:01 -0400

    my changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Eco Hounds
         </p>
+        <label for="zcode"> ZipCode: </label>
+        <input type="text" id="zcode" name="zcode"></input>
         <a
           className="App-link"
           href="https://reactjs.org"
