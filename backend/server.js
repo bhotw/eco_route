63d03c56c61871baa10a661c473f9292c51f185b
Damian <damianpreslyperera@gmail.com>
2023-04-28 13:58:21 -0400

    Added GET request dotenv not configured

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
 const express = require('express')
-const api_helper = require('./API_helper')
+const axios = require('axios').default;
 const app = express()
 const port = 4000
 require('dotenv').config()
+const API_KEY = process.env.BREEZO
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/pollen', (req, res) => {
-    api_helper.make_API_call('https://api.breezometer.com/pollen/v2/forecast/daily?lat=48.857456&lon=2.354611&days=3&key=BREEZO')
-    .then(response =>{
-        res.json(response)
-    })
-    .catch(error => {
-        res.send('Pollen API not working')
 
-    })
+var router = express.Router();
+
+
+module.exports = router;
+
+axios.get('https://api.breezometer.com/pollen/v2/forecast/daily?lat=48.857456&lon=2.354611&days=3&key=API_KEY')
+.then((response) => {
+    console.log(response.data);
     
-  })
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
