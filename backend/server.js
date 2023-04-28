338455615a722383cb81ba507fdeda77d48d90a1
Damian <damianpreslyperera@gmail.com>
2023-04-28 13:01:09 -0400

    added .env and routing for breezometer API

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
 const express = require('express')
+const api_helper = require('./API_helper')
 const app = express()
 const port = 4000
+require('dotenv').config()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/pollen', (req, res) => {
+    api_helper.make_API_call('https://api.breezometer.com/pollen/v2/forecast/daily?lat=48.857456&lon=2.354611&days=3&key=BREEZO')
+    .then(response =>{
+        res.json(response)
+    })
+    .catch(error => {
+        res.send('Pollen API not working')
+
+    })
+    
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+}) 
